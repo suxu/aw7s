85aeb1d3795498c094af48cf0e2bfe9f9c8a54b8
Administrator <suxu@ubuntu.ubuntu-domain>
2013-08-29 09:37:36 +0800

    sign up

---
+++ app/controllers/console/registrations_controller.rb
@@ -0,0 +1,24 @@
+module Console
+class RegistrationsController < Devise::RegistrationsController
+
+	layout 'console'
+
+
+	def new
+		@player = Player.new
+	end
+
+	def create
+		@player = Player.new(params.require(:player).permit(:name,:email,:password,:password_confirmation))
+		@player.phone = ""
+		if @player.save
+			sign_in("player",@player)
+     		redirect_to console_path
+		else
+			render :action => :new	
+		end
+	end
+
+end
+
+end
